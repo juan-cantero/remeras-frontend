9c2a1537a496354d68c95f687798fe4beb2efec8
juan <juan.cantero@outlook.com>
2020-11-07 11:18:33 -0300

    update home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Product from '../components/Product';
-import products from '../products';
+import ProductList from '../components/product/ProductList';
 
 const HomeScreen = () => {
   return (
     <>
       <h1 className="text-center">Remeras Nuevas</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      <ProductList />
     </>
   );
 };
